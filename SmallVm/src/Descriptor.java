e9aa7f47ca40c1e7695ce5a573f97eafea412398
egaul <eliana.gaul@gmail.com>
2023-10-21 11:18:20 -0400

    Second commit

---
+++ SmallVm/src/Descriptor.java
@@ -0,0 +1,21 @@
+public class Descriptor {
+
+    private int value;
+    private String name;
+
+    public Descriptor(){
+    }
+
+    public Descriptor(String name, int value){
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- SmallVm/src/Main.java
+++ SmallVm/src/Main.java
@@ -1,5 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        final int MAX_MEMORY_SIZE = 1000;
+
+        //Stores instructions to execute
+        ArrayList<String> instruction = new ArrayList<String>(MAX_MEMORY_SIZE);
+
+        //Stores descriptors of variables
+        ArrayList<Descriptor> descriptors = new ArrayList<Descriptor>();
+
+        // Open the file
+        FileInputStream fileIn = new FileInputStream("mySmallVm_Prog.txt");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(fileIn));
+
+        String line;
+
+        //Read File Line By Line
+        while ((line = br.readLine()) != null)   {
+            // Print the content on the console
+            instruction.add(line);
+        }
+        //Close the input stream
+        fileIn.close();
+
+
+
+
+
     }
 }
